81939a0b6ff4d6cdadf404888c355a8c954e0826
HoangTN25 <HoangTN25@fpt.com>
2025-11-13 00:02:06 +0700

    comment config CloudinaryConfig

--- src/main/java/com/hospital/backend/controller/UserController.java
+++ src/main/java/com/hospital/backend/controller/UserController.java
@@ -22,11 +22,11 @@ public class UserController {
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
-    @PostMapping(value = APIConstants.API_CREATE_USER)
-    public ResponseEntity<BaseResponse> updateUser(@RequestBody UserRequest request){
-        long beginTime = System.currentTimeMillis();
-        BaseResponse response = userService.updateUserProfile(request);
-        response.setTook(System.currentTimeMillis() - beginTime);
-        return ResponseEntity.status(response.getStatusCode()).body(response);
-    }
+//    @PostMapping(value = APIConstants.API_CREATE_USER)
+//    public ResponseEntity<BaseResponse> updateUser(@RequestBody UserRequest request){
+//        long beginTime = System.currentTimeMillis();
+//        BaseResponse response = userService.updateUserProfile(request);
+//        response.setTook(System.currentTimeMillis() - beginTime);
+//        return ResponseEntity.status(response.getStatusCode()).body(response);
+//    }
 }
